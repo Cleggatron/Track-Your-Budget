8e1276bb042a614a07a69b0b96f2d4679ac3d6ec
Cleggatron <david.robert.clegg@gmail.com>
2021-10-24 12:42:17 +0100

    wrote the webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const WebpackPWAManifest = require("webpack-pwa-manifest");
+const path = require("path")
+
+const config = {
+    entry: {
+        app: "./public/index.js",
+    },
+    output: {
+        path: __dirname + "./public/dist",
+        filename: "indexed.bundle.js"
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            }
+        ]
+    },
+    plugins: [
+        new WebpackPwaManifest({
+            fingerprints: false,
+            name: 'Budget Tracker',
+            short_name: 'Budget Tracker',
+            description: 'An application that allows you to track a budget, with offline support to handle areas where no service exists',
+            background_color: '#01579b',
+            theme_color: '#ffffff',
+            'theme-color': '#ffffff',
+            start_url: '/',
+            icons: [
+              {
+                src: path.resolve('public/icons/icon-512x512.png'),
+                sizes: [192, 512],
+                destination: path.join('assets', 'icons'),
+              },
+            ],
+          }),
+    ]
+};
+
+
+module.exports = config
