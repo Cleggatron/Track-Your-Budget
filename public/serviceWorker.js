4824d86ee89e66a8d50a780f0ebad0d8e1509aac
Cleggatron <david.robert.clegg@gmail.com>
2021-10-24 10:54:42 +0100

    corrected list of assets to cache

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,21 +1,10 @@
 const CACHE_FILES = [
     "/",
     "/index.html",
-    "/assets/style.css",
-    "/favicon.ico",    
-    "/assets/images/Angular-icon.png",
-    "/assets/images/React-icon.png",
-    "/assets/images/Vue.js-icon.png",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
-    "/assets/images/icons/icon-128x128.png",
-    "/assets/images/icons/icon-144x144.png",
-    "/assets/images/icons/icon-152x152.png",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png",
-    "/service-worker.js",
-    "/assets/js/loadPosts.js"
+    "/styles.css",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
